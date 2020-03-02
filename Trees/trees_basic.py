c565512b483841851035109f577846714c64bcec
MD RIJWAN <md1994rijwan@gmail.com>
2020-03-02 20:31:33 +0530

    Trees Basics

--- Trees/trees_basic.py
+++ Trees/trees_basic.py
@@ -1,33 +1,12 @@
-class Tree:
-    def __init__(self,root):
-        self.root = root
-        self.left = None
-        self.right = None
-    def insert(self,data):
-        if self.root:
-            if data > self.root:
-                if self.right is None:
-                    self.right = Tree(data)
-                else:
-                    self.right.insert(data)
-            elif data < self.root:
-                if self.left is None:
-                    self.left = Tree(data)
-                else:
-                    self.left.insert(data)
-        else: self.root = data
-    def display(self):
-        if self.left:
-            self.left.display()
-        print(self.root)
-        if self.right:
-            self.right.display()
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.left = left
+        self.right = right
 
-if __name__ == "__main__":
-    t = Tree(5)
-    t.insert(6)
-    t.insert(7)
-    t.insert(2)
-    t.display()
-
-        
+def insert(root,data):
+    if root is None:
+        root = Node(data)
+    else:
+        if data < root.data:
+            
